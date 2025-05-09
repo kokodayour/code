219ecf063450af673cf7be1550ddc491f9ffc999
kokodayour <3177196279@qq.com>
2025-05-09 19:11:55 +0800

    5.9

---
+++ python/leetcode/node.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+    cur = list_node
+    while cur:
+        print(f'{cur.val} ->', end=' ')
+        cur = cur.next
+    print('null')
