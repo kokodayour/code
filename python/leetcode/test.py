c3352346fe7db146bde5fdda4c46a7f348a62fe8
kokodayour <3177196279@qq.com>
2025-10-23 17:05:33 +0800

    10.23

---
+++ python/leetcode/test.py
@@ -0,0 +1,16 @@
+def permute(nums):
+    n = len(nums)
+    ans = []
+    path = [0]*n
+    def dfs(i, s):
+        if i == n:
+            ans.append(path.copy())
+            return
+        for x in s:
+            path[i] = x
+            dfs(i+1, s-{x})
+    dfs(0, set(nums))
+    return ans
+
+nums = [1,2,3]
+permute(nums)
