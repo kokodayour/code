839ee62e548e114d0d60059fc52565401a29dea5
kokodayour <3177196279@qq.com>
2025-10-29 17:50:21 +0800

    10.29

--- python/leetcode/test.py
+++ python/leetcode/test.py
@@ -1,16 +1,28 @@
-# the sum of k numbers is n
-def combinationSum3(k, n):
+def partition(s: str) -> list[list[str]]:
+    n = len(s)
     ans = []
-    path = [0]*k
-    def dfs(i):
-        if i == k:
-            if sum(path) == n:
-                ans.append(path.copy())
+    subs = []
+
+    def dfs(comma, start):
+        """
+        Args:
+            comma: the index of comma
+            start: the index of first elem of 回文串
+        """
+        if comma == n:
+            ans.append(subs.copy())
             return
-        for x in range(i, 10):
-            path[i] = x
-            dfs(i+1)
-    dfs(0)
+        # not select
+        # if comma < n-1:
+        dfs(comma+1, start)
+        # select
+        substring = s[start: comma+1]
+        if substring == substring[::-1]:
+            subs.append(substring)
+            dfs(comma+1, comma+1)
+            subs.pop()
+        
+    dfs(0, 0)
     return ans
 
-combinationSum3(2, 3)
+partition('aab')
